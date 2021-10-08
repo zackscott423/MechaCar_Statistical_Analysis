3a416c5508ef8a01f7f4406a32f048e12ca87495
zackscott423 <83609766+zackscott423@users.noreply.github.com>
2021-10-08 15:01:58 -0500

    adding lot summary

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,15 @@ head(mpg)
 lm(vehicle_length ~ mpg + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg)
 
 summary(lm(vehicle_length ~ mpg + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg))
+
+coil <- read.csv("Suspension_Coil.csv")
+
+head(coil)
+
+total_summary <- coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD=sd(PSI))
+
+total_summary
+
+lot_summary <- coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+lot_summary
